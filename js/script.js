bbaf09140dba13a631f44e9cc3f3a2cb32eaff47
vlad1010 <misha1079@ukr.net>
2025-05-12 13:36:28 +0300

    12 05 2025

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const swiper = new Swiper('.image-slider', {
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -9,4 +11,11 @@ const swiper = new Swiper('.image-slider', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    // when window width is <= 499px
+    499: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 20
+    },
+  }
 });
